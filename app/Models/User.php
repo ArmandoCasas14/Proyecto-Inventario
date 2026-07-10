b3cbef13824f629c18c45e9504a938fd7c61694c
Juan Manuel Orozco Ospina <juanmanuelorozcoospina@gmail.com>
2026-07-10 07:45:11 -0500

    feat: configura modelo User con tabla usuarios y soluciona login de breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,84 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $table = 'usuarios'; //
+
+    protected $fillable = [
+        'nombre', //
+        'email', //
+        'contraseña', //
+        'rol_id', //
+        'estado', //
+    ];
+
+    protected $hidden = [
+        'contraseña', //
+        'remember_token',
+    ];
+
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Mapea de forma interna la columna 'contraseña' como si fuera 'password' para Laravel.
      */
+    public function getAuthPassword()
+    {
+        return $this->contraseña;
+    }
+    public function getAuthName()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Truco definitivo: Cuando Laravel busque internamente $user->password, 
+     * devolverá el valor de la columna 'contraseña'.
+     */
+    public function getPasswordAttribute()
+    {
+        return $this->contraseña;
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Cuando Laravel intente guardar o actualizar $user->password,
+     * lo guardará automáticamente en la columna 'contraseña'.
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['contraseña'] = $value;
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['nombre'] = $value;
+    }
+
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
+    }
+
+    public function rol()
+    {
+        return $this->belongsTo(Rol::class, 'rol_id');
+    }
+
+    public function movimientos()
+    {
+        return $this->hasMany(Movimiento::class, 'usuario_id');
     }
 }
