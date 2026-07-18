c056f98d7945669b8de3c4a9a370525126e09164
ArmandoCasas14 <luis807.555@gmail.com>
2026-07-18 10:34:05 -0500

    ajuste del seeder archivo del entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ php artisan storage:link --force || true
 
 # Ejecutar migraciones automáticamente
 echo "Ejecutando migraciones de base de datos..."
-php artisan migrate:fresh --seed --seeder=NombreDeTuSeeder --force
+php artisan migrate:fresh --seed --seeder=CatalogoSeeder --force
 
 # Iniciar PHP-FPM en segundo plano
 php-fpm -D
