704fd4fea9a7662ecd026074b456158cf1acf5d4
ArmandoCasas14 <luis807.555@gmail.com>
2026-07-18 10:40:57 -0500

    añade npm run build

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,7 @@ php artisan migrate:fresh --seed --seeder=CatalogoSeeder --force
 
 # Iniciar PHP-FPM en segundo plano
 php-fpm -D
+npm run build
 
 # Arrancar el servidor web Nginx en primer plano
 echo "Iniciando Nginx..."
