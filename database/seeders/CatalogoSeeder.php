fceb02d7f575c994351c6fe6672e8b5a1ca22b69
ArmandoCasas14 <luis807.555@gmail.com>
2026-07-18 10:05:06 -0500

    añade el comando de seeder en el entrypoint y un seeder para usuario

--- database/seeders/CatalogoSeeder.php
+++ database/seeders/CatalogoSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Role;
 use App\Models\Category;
 use App\Models\MovementType;
+use App\Models\User;
 
 class CatalogoSeeder extends Seeder
 {
@@ -20,6 +21,14 @@ class CatalogoSeeder extends Seeder
         foreach ($roles as $rol) {
             Role::create(['name' => $rol]);
         }
+        User::firstOrCreate(
+            ['email' => 'admin@sistema.com'], 
+            [
+                'name'     => 'Administrador General',
+                'password' => Hash::make('password123'),
+                'role_id'  => 1, // Asignamos el ID del rol obtenido arriba
+            ]
+        );
 
         // 2. Categorías (Versátiles para la mayoría de inventarios)
         $categorias = ['Electrónica', 'Papelería', 'Limpieza', 'Alimentos', 'Herramientas', 'Otros'];
