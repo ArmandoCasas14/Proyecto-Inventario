a00978f6616195e2ac549f71a404d0bb7139aeb5
Juan Manuel Orozco Ospina <juanmanuelorozcoospina@gmail.com>
2026-07-22 18:57:09 -0500

    feat: Añade logica para que se muestre el logo del aplicativo en la pestaña

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Inventario') }}</title>
+        <title>{{ config('app.name', 'StockMaster') }}</title>
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         
         <!-- Fonts -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=2">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
 
